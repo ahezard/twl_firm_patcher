18a75629e0172849bc7189fbec532256eb5eb817
ahezard <ahezard@gmail.com>
2016-08-18 12:11:47 +0200

    ARM7 unlocked :-) Big thanks to Normatt

--- README.md
+++ README.md
@@ -2,19 +2,13 @@
 
 This patcher that allows to produce a special twlBg.cxi by patching the devLauncher SRL contained into it.
 
-This modifed twlBg.cxi allows to unlock part of the TWL mode Hardware (DSI) into the NTR mode (DS)
+This modifed twlBg.cxi allows to unlock the TWL mode Hardware (DSI) into the NTR mode (DS)
 
-The main advantage available is the capability to boost arm9 speed (from 66 mhz to 133 mhz) without breaking NTR games.
+This unlock new capabilitys in NTR mode without breaking games:
+-  boost arm9 speed (from 66 mhz to 133 mhz)
 
 Usage :
-- Download the twl_firm of your console
-
-Old 3DS TWL_FIRM (Firmware for DS/DSi games):
-firm: http://nus.cdn.c.shop.nintendowifi.net/ccs/download/0004013800000102/00000016
-
-New 3DS TWL_FIRM (Firmware for DS/DSi games):
-firm: http://nus.cdn.c.shop.nintendowifi.net/ccs/download/0004013820000102/00000000
-
+- Dump the twl_firm of your console using decrypt9 (SysNAND Options->Miscellanous->NCCH FIRMs Dump)
 - rename it to firmware_twl.bin and put it into input.
 - run go.cmd
 - put the twlBg.cxi file into luma/sysmodules (V6.0 dev version required)
@@ -34,24 +28,20 @@ In devLauncher SRL :
 
 ARM9 section (0x004000-0x02A000)
 - offset 0x07368 replace 08 60 87 05 by B8 60 C7 01 
-(Only this patch is really effective and needed, the other patches only change slightly the value of the 0x4004008 in ARM9 from 83000000 to 82000000)   
 
 ARM7 section (0x02A000-0x034888)
-- offset 0x2A180 replace 00 00 81 E5 by B0 00 C1 E1
 
 ARM9i section (0x055C00-0x059C00)
 
 ARM7i section (0x059C00-0x05F550)
-- offset 0xA61EC replace 00 00 85 E5 by B0 00 C5 E1
-- offset 0xA6244 replace 00 00 82 E5 by B0 00 C2 E1
-- offset 0xA6250 replace 00 00 82 15 by B0 00 C2 11
-- offset 0xA6278 replace 04 00 82 E5 by B4 00 C2 E1
-- offset 0xA6298 replace 00 10 82 E5 by B0 10 C2 E1
+- offset OxA5888 replace 00 00 85 E5 by B0 00 C5 E1
 
 All of this started from a Normatt theory so special thanks to him for the idea behind :
 - [9:26:23 PM] Normmatt: 4004008h - DSi9 - SCFG_EXT - Extended Features (R/W) [8307F100h]
 - [9:26:32 PM] Normmatt: just make sure bit31 is NEVER set
 - [9:26:42 PM] Normmatt: and boom you can re-enable all twl hardware from ntr mode
+
+Thanks again to Normatt for the ARM7 patch.
 
 Thanks as well to Apache Thunder and Shutterbug2000 for the testing and hacking support.
  
--- scripts/patchDevLauncher.py
+++ scripts/patchDevLauncher.py
@@ -100,89 +100,19 @@ filew = open(args.out, "wb")
 
 filesize=getSize(files)
 
-# 1st patch
+# 1st patch : ARM9 unlock
 srlsizeoffset=0x07368
 skipUntilAddress(files,filew,0,srlsizeoffset)
 files.read(4)
 filew.write("\xB8\x60\xC7\x01")
 current=srlsizeoffset+4
 
-# TEST 1 whitesceen
-#srlsizeoffset=0x078EE
-#skipUntilAddress(files,filew,current,srlsizeoffset)
-#files.read(4)
-#filew.write("\x19\x88\x9A\x1C")
-#current=srlsizeoffset+4
-
-# TEST 2 whitesceen
-#srlsizeoffset=0x13C66
-#skipUntilAddress(files,filew,current,srlsizeoffset)
-#files.read(4)
-#filew.write("\x10\x88\x9A\x1C")
-#current=srlsizeoffset+4
-
-# TEST 3 white screen
-#srlsizeoffset=0x2A150
-#skipUntilAddress(files,filew,current,srlsizeoffset)
-#files.read(4)
-#filew.write("\x29\x00\x00\xEB")
-#current=srlsizeoffset+4
-
-# 2nd patch
-#srlsizeoffset=0x2A180
-#skipUntilAddress(files,filew,current,srlsizeoffset)
-#files.read(4)
-#filew.write("\xB0\x00\xC1\xE1")
-#current=srlsizeoffset+4
-
-# 3rd patch
-#srlsizeoffset=0xA61EC
-#skipUntilAddress(files,filew,current,srlsizeoffset)
-#files.read(4)
-#filew.write("\xB0\x00\xC5\xE1")
-#current=srlsizeoffset+4
-
-#TEST => NO EFFECT
-#srlsizeoffset=0xA6224
-#skipUntilAddress(files,filew,current,srlsizeoffset)
-#files.read(4)
-#filew.write("\x01\x00\xA0\x03")
-#current=srlsizeoffset+4
-
-# 4th patch
-#srlsizeoffset=0xA6244
-#skipUntilAddress(files,filew,current,srlsizeoffset)
-#files.read(4)
-#filew.write("\xB0\x00\xC2\xE1")
-#current=srlsizeoffset+4
-
-# 5th patch
-#srlsizeoffset=0xA6250
-#skipUntilAddress(files,filew,current,srlsizeoffset)
-#files.read(4)
-#filew.write("\xB0\x00\xC2\x11")
-#current=srlsizeoffset+4
-
-# 6th patch
-#srlsizeoffset=0xA6278
-#skipUntilAddress(files,filew,current,srlsizeoffset)
-#files.read(4)
-#filew.write("\xB4\x00\xC2\xE1")
-#current=srlsizeoffset+4
-
-#TEST (in place of 7th) => NO EFFECT
-#srlsizeoffset=0xA6294 
-#skipUntilAddress(files,filew,current,srlsizeoffset)
-#files.read(4)
-#filew.write("\xFF\x14\x81\xE3")
-#current=srlsizeoffset+4
-
-# 7th patch
-#srlsizeoffset=0xA6298
-#skipUntilAddress(files,filew,current,srlsizeoffset)
-#files.read(4)
-#filew.write("\xB0\x10\xC2\xE1")
-#current=srlsizeoffset+4
+# 2nd patch : ARM7 unlock
+srlsizeoffset=0xA5888
+skipUntilAddress(files,filew,current,srlsizeoffset)
+files.read(8)
+filew.write("\x08\x62\x86\xE3\x08\x60\x87\xE5")
+current=srlsizeoffset+8
 
 skipUntilAddress(files,filew,current,filesize)
 
